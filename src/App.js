381745e4797e6d518a2227976a6b00bda8fd7a4b
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-11-04 12:51:05 +0600

    add-weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { ThemeProvider } from "styled-components";
-import { useState} from "react";
-import { darkTheme } from './utils/Themes.js'
+import { useState } from "react";
+import { darkTheme } from "./utils/Themes.js";
 import Navbar from "./components/Navbar";
-import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
@@ -18,19 +18,28 @@ const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
   overflow-x: hidden;
-`
+`;
 
 const Wrapper = styled.div`
-  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
-`
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 function App() {
   const [openModal, setOpenModal] = useState({ state: false, project: null });
-  console.log(openModal)
+  console.log(openModal);
   return (
     <ThemeProvider theme={darkTheme}>
-      <Router >
+      <Router>
         <Navbar />
         <Body>
           <HeroSection />
@@ -44,9 +53,9 @@ function App() {
             <Contact />
           </Wrapper>
           <Footer />
-          {openModal.state &&
+          {openModal.state && (
             <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
-          }
+          )}
         </Body>
       </Router>
     </ThemeProvider>
--- src/data/constants.js
+++ src/data/constants.js
@@ -23,8 +23,7 @@ export const skills = [
     skills: [
       {
         name: "React Js",
-        image:
-          "/react.webp",
+        image: "/react.webp",
       },
       {
         name: "Redux",
@@ -33,8 +32,7 @@ export const skills = [
       },
       {
         name: "Next Js",
-        image:
-          "/next.webp",
+        image: "/next.webp",
       },
       {
         name: "HTML",
@@ -57,49 +55,45 @@ export const skills = [
       },
       {
         name: "Material UI",
-        image:
-          "/mui.webp",
+        image: "/mui.webp",
       },
-          {
+      {
         name: "Framer-motion",
-        image:
-          "/motion.png",
+        image: "/motion.png",
       },
-            {
+      {
         name: "Zustand",
-        image:
-          "/zustand.png",
+        image: "/zustand.png",
       },
-          {
+      {
         name: "Material UI",
-        image:
-          "/mui.webp",
+        image: "/mui.webp",
       },
       {
         name: "Tailwind",
-        image:"/tailwind.webp",
+        image: "/tailwind.webp",
       },
       {
-        name:"ShadCn",
-        image:"/shad.webp"
-      }
+        name: "ShadCn",
+        image: "/shad.webp",
+      },
     ],
   },
   {
     title: "Backend",
     skills: [
       {
-        name:"Node.js",
-        image : "/node.webp"
+        name: "Node.js",
+        image: "/node.webp",
       },
-{
-name:"Express.js",
-image :"/express.webp"
-},
-{
-name:"MongoDb",
-image :"/mongodb.webp"
-},
+      {
+        name: "Express.js",
+        image: "/express.webp",
+      },
+      {
+        name: "MongoDb",
+        image: "/mongodb.webp",
+      },
       {
         name: "Python",
         image:
@@ -114,7 +108,7 @@ image :"/mongodb.webp"
         name: "Postgresql",
         image: "https://www.postgresql.org/media/img/about/press/elephant.png",
       },
-    
+
       {
         name: "Firebase",
         image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
@@ -127,8 +121,7 @@ image :"/mongodb.webp"
     skills: [
       {
         name: "Git",
-        image:
-          "/git.webp",
+        image: "/git.webp",
       },
       {
         name: "GitHub",
@@ -152,30 +145,28 @@ image :"/mongodb.webp"
       },
       {
         name: "Postman",
-        image:
-          "/postman.webp",
+        image: "/postman.webp",
       },
       {
         name: "Vercel",
-        image:
-          "/vercel.webp",
+        image: "/vercel.webp",
       },
       {
         name: "Figma",
-        image:
-          "/figma.webp",
+        image: "/figma.webp",
       },
     ],
   },
 ];
 
 export const experiences = [
-  , {
+  ,
+  {
     id: 1,
-    img:"/innovia.webp",
+    img: "/innovia.webp",
     role: "Front End Developer",
     company: "Innovia Tech",
-    date: "June 2025 - continue",
+    date: "June 2025 - November 2025 (6 month contract)",
     desc: "I am  working here as a jr. developer.",
     skills: [
       "Html",
@@ -191,15 +182,15 @@ export const experiences = [
       "Zod",
       "Next.Js",
       "Node.js, Express.js",
-      "MongoDb"
-    ]
+      "MongoDb",
+    ],
   },
   {
     id: 0,
-    img:"/si.png",
+    img: "/si.png",
     role: "Front End Developer",
     company: "Software Innovation",
-    date: "Oct 2024 - May 2025",
+    date: "April 2024 - May 2025",
     desc: "I worked here as a Jr. Developer.",
     skills: [
       "Html",
@@ -214,27 +205,9 @@ export const experiences = [
       "Zod",
       "Next.Js",
       "Node.js, Express.js",
-      "MySql"
-    ]
-    
+      "MySql",
+    ],
   },
-   {
-    id: 2,
-    img:"/byte.webp",
-    role: "Front End Developer",
-    company: "Bytenyx Ltd",
-    date: "6 months (company was closed)",
-    desc: "I worked here as a Intern Developer.",
-    skills: [
-      "Html",
-      "Css",
-      "Bootstrap",
-      "Tailwind",
-      "JavaScript",
-      "TypeScript",
-      "React.Js"
-    ]
-  }
 ];
 
 export const education = [
@@ -260,27 +233,43 @@ export const education = [
 
 export const projects = [
   {
- title: "Complete E-commerce Production Ready Website",
+    title: "Complete E-commerce Production Ready Website",
     description:
       "Complete E-commerce Production Ready Website — A fully functional e-commerce platform built with Next.js and Tailwind CSS for a modern UI, powered by Redux Toolkit and RTK Query for state management and data fetching. It integrates Formik for seamless form handling and SWR for client-side caching. The backend is developed with Node.js and Express, connected to MongoDB for scalable database management. This project is production-ready, ensuring high performance, scalability, and maintainability for real-world use. Note:Source-code can't provide. this is our company policy",
-    image:
-      "/alarafat.png",
-    tags: ["Next js","Tailwind","Redux-toolkit","Rtk-query","Fromik", "SWR", "Node", "Express", "MongoDb"],
+    image: "/alarafat.png",
+    tags: [
+      "Next js",
+      "Tailwind",
+      "Redux-toolkit",
+      "Rtk-query",
+      "Fromik",
+      "SWR",
+      "Node",
+      "Express",
+      "MongoDb",
+    ],
     category: "web app",
     webapp: "https://alarafatoasis.com/",
   },
-// ---------------------------------
-    {
- title: "Interprise Lavel Production Grade Blog Application",
+  // ---------------------------------
+  {
+    title: "Interprise Lavel Production Grade Blog Application",
     description:
       "This project is a production-ready Blog application built with Next.js and Tailwind CSS for a modern, responsive UI, enhanced by shadcn components and Framer Motion animations with clip-path effects. State management and data fetching are handled by Redux Toolkit with RTK Query, while the backend is powered by NestJS and MongoDB for scalability. It includes secure JWT-based authentication, optimized database structures, and reusable design tokens with motion variants. The system prioritizes accessibility, SEO, security, and testing, delivering a fast, maintainable, and feature-rich blogging platform ready for real-world use.",
-    image:
-      "/innovia.png",
-    tags: ["Next js","Tailwind","Framer-motion", "Shadcn", "SWR", "Nets js", "MongoDb"],
+    image: "/innovia.png",
+    tags: [
+      "Next js",
+      "Tailwind",
+      "Framer-motion",
+      "Shadcn",
+      "SWR",
+      "Nets js",
+      "MongoDb",
+    ],
     category: "web app",
     webapp: "https://innoviaedge.com/",
   },
-  
+
   // ---------------------------------
 
   {
@@ -288,28 +277,40 @@ export const projects = [
     title: "Complete Admin B2b",
     description:
       "This is a comprehensive admin dashboard and B2B web application, featuring dynamic forms, user management tables, and an interactive analytics dashboard. Built using Next.js, TypeScript, Tailwind CSS, Redux Toolkit, and RTK Query, it ensures strong performance, efficient state management, and scalability. Form handling and validation are powered by React Hook Form and Zod. As this is an ongoing internal project for our company, the source code is private and cannot be shared at this time.",
-    image:
-      "/b2b.png",
-    tags: ["Next js","Typescript","Tailwind","Redux-toolkit","Rtk-query","React Hook Form","Zod"],
+    image: "/b2b.png",
+    tags: [
+      "Next js",
+      "Typescript",
+      "Tailwind",
+      "Redux-toolkit",
+      "Rtk-query",
+      "React Hook Form",
+      "Zod",
+    ],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/CompleteAdminDashboard",
     webapp: "https://complete-admin-dashboard.vercel.app/",
-   
   },
+  // ---------------------------------
+  {
+    title: "Weather App",
+    description:
+      "Weather App – Real-Time Forecast & Insights .Build a modern, responsive Weather App that allows users to search any city and get accurate, up-to-date weather information. The app displays current weather, feels like temperature, humidity, wind speed, and precipitation, along with country names from ISO codes. Users can view a 7-day forecast and hourly predictions with daily high/low temperatures and weather icons for easy visualization. Features include autocomplete search, global loader during API calls, and toast notifications for success or error using Sonner. State is efficiently managed with Zustand, ensuring a smooth, interactive experience. This app combines usability and modern design to deliver an engaging weather experience.",
+    image: "/weather.png",
+    tags: ["Next js", "Tailwind", "Open-api", "Shadcn", "Zustand"],
+    category: "web app",
+    github: "https://github.com/mahfujulhoque12/weather",
+    webapp: "https://weather-one-puce.vercel.app/",
+  },
+
+  // ---------------------------------
   {
     id: 12,
     title: "E-commerce Website",
     description:
       "This is a fully functional and responsive e-commerce website developed using Next.js, TypeScript, and Tailwind CSS. The platform allows users to browse products, add items to their cart, wishlist, and compare products with ease. The modern design and intuitive user interface offer a seamless shopping experience across all devices. It’s built with performance, scalability, and maintainability in mind",
-    image:
-      "/ecom.png",
-    tags: [
-      
-      "Tailwind",
-      "NextJs",
-      "Typescript",
-
-    ],
+    image: "/ecom.png",
+    tags: ["Tailwind", "NextJs", "Typescript"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/fpl-e-commerce",
     webapp: "https://fpl-e-commerce.vercel.app/",
@@ -319,15 +320,8 @@ export const projects = [
     title: "Architecture Website",
     description:
       "This is a modern and responsive architecture website developed for Floor Planner Ltd., an architecture company. Built with Next.js, TypeScript, and Tailwind CSS, the site combines clean design with a blog-style layout to showcase architectural projects, company information, and services. It’s optimized for performance and user experience, providing a professional digital presence for the client. The website is fully live and accessible to the public",
-    image:
-      "/arc.png",
-    tags: [
-      
-      "Tailwind",
-      "NextJs",
-      "Typescript",
-
-    ],
+    image: "/arc.png",
+    tags: ["Tailwind", "NextJs", "Typescript"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/fpl-website",
     webapp: "https://fpl-new.vercel.app/",
@@ -337,49 +331,30 @@ export const projects = [
     title: "Zoom-clonig",
     description:
       "This project is a real-time video conferencing web application inspired by Zoom. Developed using Next.js, Tailwind CSS, HTML, Clerk for authentication, and Stream for video and audio streaming, it replicates core Zoom functionalities such as video calls, screen sharing, and meeting rooms. The interface is clean, responsive, and designed for smooth user interaction. It demonstrates the integration of modern third-party tools to create a scalable and collaborative communication platform",
-    image:
-      "/zoom.png",
-    tags: [
-      "Html",
-      "Tailwind",
-      "NextJs",
-      "Stream",
-      "clerk"
-    ],
+    image: "/zoom.png",
+    tags: ["Html", "Tailwind", "NextJs", "Stream", "clerk"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/Zoom-clone",
     webapp: "https://zoom-clone-lime-omega.vercel.app/",
   },
-    {
+  {
     id: 15,
     title: "Software Company Website",
     description:
       "This is a modern and responsive website developed for a software company. Built using Next.js, TypeScript, and Tailwind CSS, the project ensures high performance, type safety, and a clean user interface. State management and data fetching are efficiently handled using Redux Toolkit and RTK Query, making the application scalable and maintainable. The layout is optimized for user experience and designed to reflect a professional tech brand",
-    image:
-      "/software.png",
-    tags: [
-      "Next.js",
-      "TypeScript",
-      "Tailwind",
-      "Redux Toolkit",
-      "RTK Query"
-    ],
+    image: "/software.png",
+    tags: ["Next.js", "TypeScript", "Tailwind", "Redux Toolkit", "RTK Query"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/software-innovations/",
     webapp: "https://software-innovations.vercel.app/",
   },
-      {
+  {
     id: 16,
     title: "Doctor Personal Portfolio",
     description:
       "This is a sleek and responsive personal portfolio website designed for a medical professional. Built with Next.js, TypeScript, and Tailwind CSS, it delivers fast performance, strong type safety, and a modern, clean user interface. The site effectively showcases the doctor’s expertise, background, and services, while maintaining a professional and trustworthy look suitable for healthcare branding. Its responsive design ensures an optimal experience across all devices.",
-    image:
-      "/doctor.png",
-    tags: [
-      "Next.js",
-      "TypeScript",
-      "Tailwind",
-    ],
+    image: "/doctor.png",
+    tags: ["Next.js", "TypeScript", "Tailwind"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/doctor-protfolio",
     webapp: "https://doctor-protfolio.vercel.app/",
@@ -389,15 +364,8 @@ export const projects = [
     title: "Admin-Dashboard",
     description:
       "This is a fully functional  admin dashboard built with React, HTML, CSS, and Material-UI (MUI) for modern UI components. It includes various features such as analytics widgets, data tables, user management, and form elements. ESLint is integrated to maintain clean and consistent code quality. The dashboard is designed for usability and scalability, making it suitable for managing data-driven applications or internal admin tools",
-    image:
-      "/admin.png",
-    tags: [
-      "Html",
-      "Css",
-      "React",
-      "Eslint",
-      "MI-UI"
-    ],
+    image: "/admin.png",
+    tags: ["Html", "Css", "React", "Eslint", "MI-UI"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/React-admin-dashboard",
     webapp: "https://joyful-starlight-58bec7.netlify.app/",
@@ -407,16 +375,8 @@ export const projects = [
     title: "GitHub User",
     description:
       "GitHub User is a responsive web application that allows users to log in and view detailed GitHub user profiles. After authentication, users can explore GitHub data such as repositories, followers, following, and other profile information. Built with React, Redux for state management, Styled Components for scoped styling, and ESLint for code quality, the app provides a clean and intuitive interface. It’s a great example of integrating external APIs with a dynamic frontend. ",
-    image:
-      "/github.png",
-    tags: [
-      "Html",
-      "Css",
-      "React",
-      "Eslint",
-      "Redux",
-      "styled component"
-    ],
+    image: "/github.png",
+    tags: ["Html", "Css", "React", "Eslint", "Redux", "styled component"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/GitHub-User",
     webapp: "https://react-github-user12.netlify.app/login",
@@ -426,26 +386,19 @@ export const projects = [
     title: "IMDb Clone",
     description:
       "This is a fully responsive movie browsing web application inspired by IMDb. Built with Next.js, HTML, and Tailwind CSS, the app allows users to search movies, view top-rated and trending titles, and toggle between dark mode and light mode for an enhanced user experience. The interface is clean, fast, and user-friendly, making it easy to explore a wide range of movie content. It demonstrates API integration, theming, and responsive layout design.",
-    image:
-      "/imdb.png",
-    tags: ["Next js","Html","Tailwind"],
+    image: "/imdb.png",
+    tags: ["Next js", "Html", "Tailwind"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/Next-IMDb-clone",
     webapp: "https://next-im-db-clone-nxt.vercel.app/",
-   
   },
   {
     id: 1,
     title: "Tax App",
     description:
       "Text App is web Text Calculator. You can calculate you text using this app. note: this is a demo. not real :)",
-    image:
-      "/tax.png",
-    tags: [
-      "Next js",
-      "Bootstrap",
-      "SCSS",
-    ],
+    image: "/tax.png",
+    tags: ["Next js", "Bootstrap", "SCSS"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/Tax-app",
     webapp: "https://tax-app-five.vercel.app/",
@@ -455,22 +408,19 @@ export const projects = [
     title: "Next Ajency",
     description:
       "This is a next js app. it's look like a blog/agency projects.",
-    image:
-      "/azency.png",
-    tags: ["Next js ","Tailwind"],
+    image: "/azency.png",
+    tags: ["Next js ", "Tailwind"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/Nextjs-agency-project",
     webapp: "https://test-alpha-sandy-88.vercel.app/",
-
   },
   {
     id: 3,
     title: "Find City Temperature",
     description:
       "This is a city temperature application, you can find your city temperature in the application, :)",
-    image:
-      "/temp.png",
-    tags: ["Html","Tailwind","javascript",],
+    image: "/temp.png",
+    tags: ["Html", "Tailwind", "javascript"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/Find-City-Temperature",
     webapp: "https://find-city-temperature.vercel.app/",
@@ -480,14 +430,10 @@ export const projects = [
     title: "Banking App",
     description:
       "This is bankin app. you can diposite your money and aslo you can widrow your money.....mail:badon@don.com, and pass: 12345, so lets checl :)",
-    image:
-      "/bank.png",
+    image: "/bank.png",
     tags: ["Html", "JavaScript", "Tailwind"],
     category: "web app",
     github: "https://github.com/mahfujulhoque12/Simple-banking-system",
     webapp: "https://bank08.netlify.app/",
   },
- 
-
 ];
-
